7cb3656841210b6c4ecdf9a58e6ab47f5dfc7828
Mahbub <mmhasaneee@gmail.com>
2019-01-23 18:56:38 +0600

    course date info table from action to get form post and add page name hidden field

--- admin/partials/course-dates.php
+++ admin/partials/course-dates.php
@@ -9,7 +9,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<h1 class="wp-heading-inline">Course Dates</h1>
 	<hr class="wp-header-end">
 	<div class="tbs-courses-list-wrap">
-		<form  method="post" action="<?php echo TBS_Admin_Course_Date_Info::url(); ?>" id="tbs-course-filter">
+		<form  method="get" action="<?php echo TBS_Admin_Course_Date_Info::url(); ?>" id="tbs-course-filter">
+            <input type="hidden" name="page" value="tbs-course-date-info" />
 			<?php $this->list_table->display() ?>
 		</form>
 	</div>
