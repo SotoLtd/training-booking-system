cad2b0b1856a87f83f97284027a6513937df495e
Mahbub <mmhasaneee@gmail.com>
2019-03-06 20:06:55 +0600

    Change tag and wording of atc popup

--- public/partials/cart/added-to-cart-lightbox.tpl.php
+++ public/partials/cart/added-to-cart-lightbox.tpl.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<span id="tts-atclb-overlay" class="tts-atc-ligtbox-close"></span>
 	<div id="tts-atc-lightbox-inner">
 		<div id="tts-atc-lightbox-content">
-			<h3>Thank you for booking your course with us.</h3>
+			<p class="tts-atc-lightbox-title">It's in the basket - would you like to...</p>
 			<div class="tts-atc-lightbox-butons clearfix">
 				<a class="tts-button  button-with-arrow tts-button tts-atc-ligtbox-close" href="#">Continue shopping</a>
 				<a class="tts-button button-with-arrow ts-button tts-atc-view-cart" href="<?php echo wc_get_cart_url(); ?>">View basket</a>
